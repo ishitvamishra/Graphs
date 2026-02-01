065e34fbd678dd2c9de79edeb8081bdb510533b3
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-02 00:45:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    public long countFriendsPairings(int n) {
+        // code here
+        if(n==1 || n==2) return n;
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i<=n;i++){
+            dp[i] = dp[i-1] + (i-1)*dp[i-2];
+        }
+        return dp[n];
+    }
+}
