c152a667b8f4b0aa8f276b3622f11c0f74cb20c5
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-21 20:53:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Word Ladder II/word-ladder-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    String b;
+    HashMap<String, Integer> mpp;
+    ArrayList<ArrayList<String>> ans;
+
+    public void dfs(String word, List<String> seq){
+        if(word.equals(b)){
+            ArrayList<String> dup = new ArrayList<>(seq);
+            Collections.reverse(dup);
+            ans.add(dup);
+            return;
+        }
+
+        int steps = mpp.get(word);
+        int sz = word.length();
+        for(int i = 0; i < sz; i++){
+            for(char ch = 'a'; ch <= 'z'; ch++){
+                char replacedArray[] = word.toCharArray();
+                replacedArray[i] = ch;
+                String replacedWord = new String(replacedArray);
+                if(mpp.containsKey(replacedWord) && mpp.get(replacedWord)+1 == steps){
+                    seq.add(replacedWord);
+                    dfs(replacedWord, seq);
+                    seq.remove(seq.size()-1);
+                }
+            }
+        }
+    }
+    public ArrayList<ArrayList<String>> findSequences(String beginWord,String endWord,String[] wordList) {
+        Set<String> st = new HashSet<>();
+        for(int i = 0; i < wordList.length; i++){
+            st.add(wordList[i]);
+        }
+
+        Queue<String> q = new LinkedList<>();
+        b = beginWord;
+        q.add(beginWord);
+        mpp = new HashMap<>();
+        mpp.put(beginWord, 1);
+        int sizee = beginWord.length();
+        st.remove(beginWord);
+        while(!q.isEmpty()){
+            String word = q.peek();
+            int steps = mpp.get(word);
+            q.remove();
+            if(word.equals(endWord)) break;
+            for(int i = 0; i < sizee; i++){
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    char replacedArray[] = word.toCharArray();
+                    replacedArray[i] = ch;
+                    String replacedWord = new String(replacedArray);
+                    if(st.contains(replacedWord)){
+                        q.add(replacedWord);
+                        st.remove(replacedWord);
+                        mpp.put(replacedWord, steps+1);
+                    }
+                }
+            }
+        }
+
+        ans = new ArrayList<>();
+        if(mpp.containsKey(endWord)){
+            ArrayList<String> seq = new ArrayList<>();
+            seq.add(endWord);
+            dfs(endWord, seq);
+        }
+        return ans;
+    }
+}
