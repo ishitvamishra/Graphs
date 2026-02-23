45d9a41aff77b7284c28173bfdbdf443f9ffe98e
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-23 23:42:57 +0530

    Time: 8 ms (30.82%) | Memory: 47.3 MB (50.05%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -3,6 +3,7 @@ class Solution {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
 
+        //Build Adj List
         for(int i = 0; i < V; i++){
             adj.add(new ArrayList<>());
         }
@@ -10,9 +11,11 @@ class Solution {
             int u = edge[0];
             int v = edge[1];
 
-            adj.get(u).add(v);
+            //to take u you need to complete v first so v -> u
+            adj.get(v).add(u);
         }
 
+        //Calculate indegree of each vertex
         int[] indegree = new int[V];
         for(int i = 0; i < V; i++){
             for(int it :  adj.get(i)){
@@ -20,18 +23,21 @@ class Solution {
             }
         }
 
+        //add all element whose initial indegree is 0
         for(int i = 0; i < V; i++){
             if(indegree[i] == 0){
                 q.add(i);
             }
         }
 
+        //Build topo array
         int[] topo = new int[V];
-        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
         while(!q.isEmpty()){
             int node = q.poll();
-            ans.add(node);
+            topo[i++] = node;
 
+            //for each node add to ans, reduce the indegree of all its neighbouring nodes
             for(int it : adj.get(node)){
                 indegree[it]--;
                 if(indegree[it] == 0){
@@ -40,7 +46,6 @@ class Solution {
             }
         }
 
-        if(ans.size() == V) return true;
-        return false;
+        return i == V;
     }
 }
