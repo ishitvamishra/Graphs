48d978a365e2f9d484f22d746f1701cfe59c65c9
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-28 03:16:59 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (99.37%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -2,16 +2,15 @@ class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
         for(int op = 0; op < k; op++){
             int min = Integer.MAX_VALUE;
+            int minIdx = 0;
             for(int i = 0;  i < nums.length; i++){
-                min = Math.min(min, nums[i]);
-            }
-
-            for(int i = 0; i < nums.length; i++){
-                if(nums[i] == min){
-                    nums[i] = nums[i]*multiplier;
-                    break;
+                if(nums[i] < min){
+                    minIdx = i;
+                    min = nums[i];
                 }
             }
+
+            nums[minIdx] = nums[minIdx]*multiplier;
         }
         return nums;
     }
