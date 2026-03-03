bd5c155df93b1648a6b6d559e146dcfb5f7c8314
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-04 02:51:59 +0530

    Time: 36 ms (89.42%) | Memory: 47.3 MB (70.74%) - LeetSync

---
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.java
@@ -0,0 +1,61 @@
+class State{
+    int city;
+    int cost;
+    int time;
+
+    State(int city, int cost, int time){
+        this.city = city;
+        this.cost = cost;
+        this.time = time;
+    }
+}
+
+class Solution {
+    public int minCost(int maxTime, int[][] edges, int[] passingFees) {
+        int n = passingFees.length;
+
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            adj.get(u).add(new int[]{v,w});
+            adj.get(v).add(new int[]{u,w});
+        }
+
+        PriorityQueue<State> pq = new PriorityQueue<>((x,y) -> x.cost - y.cost);
+        int minTime[] = new int[n];
+        Arrays.fill(minTime, Integer.MAX_VALUE);
+
+        pq.add(new State(0, passingFees[0], 0));
+        minTime[0] = 0;
+
+        while(!pq.isEmpty()){
+            int city = pq.peek().city;
+            int cost = pq.peek().cost;
+            int time = pq.peek().time;
+            pq.remove();
+
+            if(city == n-1) return cost;  //dest reached
+
+            for(int[] it : adj.get(city)){
+                int nextCity = it[0];
+                int travelTime = it[1];
+                int newTime = time + travelTime;
+
+                if(newTime > maxTime) continue;
+
+                if(newTime < minTime[nextCity]){
+                    minTime[nextCity] = newTime;
+                    pq.add(new State(nextCity, cost + passingFees[nextCity], newTime));
+                }
+            }
+        }
+        return -1;
+    }
+}
