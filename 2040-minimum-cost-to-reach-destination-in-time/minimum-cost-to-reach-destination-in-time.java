492dd31846bd2e08d4378c8eb45a42db98326c8a
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-05 16:58:48 +0530

    Time: 38 ms (84.02%) | Memory: 47.2 MB (87.64%) - LeetSync

--- 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.java
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.java
@@ -19,6 +19,7 @@ class Solution {
             adj.add(new ArrayList<>());
         }
 
+        //Build adj list
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
@@ -28,6 +29,8 @@ class Solution {
             adj.get(v).add(new int[]{u,w});
         }
 
+        //Apply simple dijkstras algorithm, just one thing  to take care that an additional 
+        //parameter of cost is also added so we well need to carry that as well 
         PriorityQueue<State> pq = new PriorityQueue<>((x,y) -> x.cost - y.cost);
         int minTime[] = new int[n];
         Arrays.fill(minTime, Integer.MAX_VALUE);
@@ -41,14 +44,14 @@ class Solution {
             int time = pq.peek().time;
             pq.remove();
 
-            if(city == n-1) return cost;  //dest reached
+            if(city == n-1) return cost;  //dest reached, return cost
 
             for(int[] it : adj.get(city)){
                 int nextCity = it[0];
                 int travelTime = it[1];
                 int newTime = time + travelTime;
 
-                if(newTime > maxTime) continue;
+                if(newTime > maxTime) continue; //constraint voilation
 
                 if(newTime < minTime[nextCity]){
                     minTime[nextCity] = newTime;
