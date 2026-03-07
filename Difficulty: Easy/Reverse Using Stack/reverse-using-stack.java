527c258e7426f4ef21c4df56af47515b9558fe71
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-07 20:05:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String reverse(String S) {
+        // code here
+        StringBuilder reversed = new StringBuilder(S).reverse();
+        return reversed.toString();
+    }
+}
