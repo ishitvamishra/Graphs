f2b396f02c13afa9aaf556ee1a0a596600cdad86
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-09 23:35:04 +0530

    Time: 103 ms (20.22%) | Memory: 47 MB (12.63%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numSquares(int n) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n+1];
+
+        List<Integer> perfect = new ArrayList<>();
+        for(int i = 1; i * i <= n; i++){
+            perfect.add(i*i);
+        }
+
+        q.add(n);
+        vis[n] = true;
+
+        int steps = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for(int i = 0; i < size; i++){
+                int x = q.poll();
+
+                if(x == 0) return steps;
+
+                for(int p : perfect){
+                    if(p <= x){
+                        int num = x -  p;
+
+                        if(!vis[num]){
+                            q.add(num);
+                            vis[num] = true;
+                        }
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+}
