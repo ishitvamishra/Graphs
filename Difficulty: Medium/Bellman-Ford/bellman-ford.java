ceef262a58d1c264ea5426a6614727efb8f3d14f
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-11 00:56:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -0,0 +1,36 @@
+// User function Template for Java
+
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int)1e8);
+        dist[src] = 0;
+        
+        for(int i = 0; i < V-1; i++){
+            for(int[] edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                int[] temp = new int[1];
+                temp[0] = -1;
+                return temp;
+            }
+        }
+        
+        return dist;
+    }
+}
