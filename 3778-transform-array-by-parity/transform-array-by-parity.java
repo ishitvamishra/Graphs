5ad8ffd1dd3152be14997f1ae108fc22ebd36ea7
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-03-16 23:08:58 +0530

    Time: 5 ms (37.42%) | Memory: 47.1 MB (40.12%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -2,10 +2,7 @@ class Solution {
     public int[] transformArray(int[] nums) {
         int[] res = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 0){
-                res[i] = 0;
-            }
-            else{
+            if(nums[i] % 2 != 0){
                 res[i] = 1;
             }
         }
