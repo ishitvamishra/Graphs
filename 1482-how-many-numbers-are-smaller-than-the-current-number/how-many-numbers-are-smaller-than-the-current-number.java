96ad6539500c3c50d1b7628426a95b5e926cf7d6
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-04-08 13:37:41 +0530

    Time: 1 ms (99.83%) | Memory: 45.5 MB (76.33%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-
-        for(int i = 0; i < n; i++){
-            int cnt = 0;
-            for(int j = 0; j < n; j++){
-                if(i != j && nums[i] > nums[j]){
-                    cnt++;
-                }
-            }
-            res[i] = cnt;
+        //Counting sort
+        //Count freq of all elements within constraints
+        int[] freq = new int[101];
+        
+        for (int num : nums) {
+            freq[num]++;
         }
-
-        return res;
+        
+        //Comute sum of freq all smaller elements than index i
+        for (int i = 1; i < 101; i++) {
+            freq[i] += freq[i - 1];
+        }
+        
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                result[i] = freq[nums[i] - 1];
+            }
+        }
+        
+        return result;
     }
 }
