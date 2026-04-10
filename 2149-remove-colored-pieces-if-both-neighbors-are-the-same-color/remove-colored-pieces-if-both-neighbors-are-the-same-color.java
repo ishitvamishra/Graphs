f14c8993411ce609ae5bf48f2caa75abdebd77f4
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-04-11 01:51:59 +0530

    Time: 7 ms (100.00%) | Memory: 47.5 MB (80.21%) - LeetSync

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int consA = 0, consB = 0;
+        int chanceA = 0, chanceB = 0;
+
+        for(char ch : colors.toCharArray()){
+            if(ch == 'A'){
+                consB = 0;
+                consA++;
+                if(consA >= 3) chanceA++;
+            }else{
+                consA = 0;
+                consB++;
+                if(consB >= 3) chanceB++;
+            }
+        }
+
+        return chanceA > chanceB;
+    }
+}
