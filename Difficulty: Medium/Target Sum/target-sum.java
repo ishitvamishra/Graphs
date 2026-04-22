cadc4e1b875be76cca5ca74ff7ebfa0f886d1fce
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-04-23 00:23:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Target Sum/target-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static int findWays(int num[], int tar) {
+        int n = num.length;
+        int[][] dp = new int[n][tar + 1];
+
+        // Base case
+        if (num[0] == 0) {
+            dp[0][0] = 2;
+        } else {
+            dp[0][0] = 1;
+        }
+
+        if (num[0] != 0 && num[0] <= tar) {
+            dp[0][num[0]] = 1;
+        }
+
+        for(int ind = 1; ind < n; ind++){
+            for(int sum = 0; sum <= tar; sum++){
+                int notTake = dp[ind - 1][sum];
+                int take = 0;
+
+                if(num[ind] <= sum){
+                    take = dp[ind - 1][sum - num[ind]];
+                }
+
+                dp[ind][sum] = (take + notTake);
+            }
+        }
+
+        return dp[n-1][tar];
+    }
+    
+    public int totalWays(int[] nums, int d) {
+        // code here
+        int totSum = 0;
+
+        for(int it : nums) totSum += it;
+
+        if(totSum - d < 0 || (totSum - d) % 2 != 0) return 0;
+
+        return findWays(nums, (totSum - d) / 2);
+    }
+}
