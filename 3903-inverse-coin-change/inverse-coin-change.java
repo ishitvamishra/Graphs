83dbac35e7766396b6a4e870159af83bacd0e99b
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-04-23 23:25:49 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (78.36%) - LeetSync

---
+++ 3903-inverse-coin-change/inverse-coin-change.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> findCoins(int[] numWays) {
+        int n = numWays.length;
+        
+        //dp[i] = current coins ke saath amount i banane ke total ways
+        long[] dp = new long[n + 1];
+        dp[0] = 1; // kuch bhi mat lo toh 0 ban jayega
+
+        List<Integer> coins = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+
+            if(dp[i] > numWays[i - 1]){
+                return new ArrayList<>();
+            }
+
+            if(dp[i] < numWays[i - 1]){
+                coins.add(i);
+
+                for(int j = i; j <= n; j++){
+                    dp[j] += dp[j  - i];
+                }
+
+                if (dp[i] != numWays[i - 1]) {
+                    return new ArrayList<>();
+                }
+            }
+        }
+
+        return coins;
+    }
+}
