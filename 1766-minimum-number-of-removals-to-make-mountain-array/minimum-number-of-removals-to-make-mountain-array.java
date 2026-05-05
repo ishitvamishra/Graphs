b615560a336a47d0338e59f91e36694b327ae2a8
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-05 23:37:22 +0530

    Time: 43 ms (18.18%) | Memory: 46.4 MB (61.28%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] dp1 = new int[n];
+        for(int i = 0; i < n; i++){
+            dp1[i] = 1;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i] && dp1[j] + 1 > dp1[i]){
+                    dp1[i] = dp1[j] + 1;
+                }
+            }
+        }
+        
+        int[] dp2 = new int[n];
+        for(int i = 0; i < n; i++){
+            dp2[i] = 1;
+        }
+        
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = n - 1; j > i; j--){
+                if(nums[j] < nums[i] && dp2[j] + 1 > dp2[i]){
+                    dp2[i] = dp2[j] + 1;
+                }
+            }
+        }
+        
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            if(dp1[i] > 1 && dp2[i] > 1){
+                max = Math.max(max, dp1[i] + dp2[i] - 1);
+            }
+        }
+        
+        return n - max;
+    }
+}
