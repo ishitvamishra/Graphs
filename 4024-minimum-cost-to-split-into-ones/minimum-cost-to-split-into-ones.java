8a60ab4c31fd69267a6a551d70294b575eadc1b7
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-07 01:45:26 +0530

    Time: 1 ms (38.21%) | Memory: 43 MB (11.03%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minCost(int n) {
-        int[] dp = new int[n+1];
+        int dp[] = new int[n+1];
 
         Arrays.fill(dp, (int)1e9);
-        
         dp[1] = 0;
-        
 
         for(int i = 2; i <= n; i++){
-            for(int j = 1; j < i; j++){
-                int cost = dp[j] + dp[i - j] + j*(i-j);
-                dp[i] = Math.min(dp[i], cost);
-            }
+            dp[i] = dp[i - 1] + i - 1;
         }
 
         return dp[n];
