be9871357b73169e89e4f8d0187cb666565387d4
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-06 13:59:13 +0530

    Time: 1 ms (99.48%) | Memory: 168.8 MB (46.17%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -3,22 +3,21 @@ class Solution {
         int n = triplets.length;
 
         boolean tarA = false, tarB = false, tarC = false;
-
-
+        int x =  target[0], y = target[1], z = target[2];
 
         for(int i = 0; i < n; i++){
             int a = triplets[i][0];
             int b = triplets[i][1];
             int c = triplets[i][2];
 
-            if(a > target[0] || b > target[1] || c > target[2]) continue;
+            if(a > x || b > y|| c > z) continue;
 
-            if(a == target[0]) tarA = true;
-            if(b == target[1]) tarB = true;
-            if(c == target[2]) tarC = true;
+            if(a == x) tarA = true;
+            if(b == y) tarB = true;
+            if(c == z) tarC = true;
+
+            if(tarA && tarB && tarC) return true;
         }
-
-        if(tarA && tarB && tarC) return true;
 
         return false;
     }
