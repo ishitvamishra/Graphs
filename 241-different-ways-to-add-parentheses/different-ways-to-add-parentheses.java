1fd79fe659337d7a1ef7a43ca41db1a579a596ab
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-08 00:10:59 +0530

    Time: 2 ms (86.43%) | Memory: 43.5 MB (78.57%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -4,18 +4,22 @@ class Solution {
 
     public List<Integer> diffWaysToCompute(String expr) {
 
+        //Store for overlapping expressiom
         if(memo.containsKey(expr)) return memo.get(expr);
 
+        //for each fn call, a new list is made 
         List<Integer> ans = new ArrayList<>();
 
         for(int i = 0; i < expr.length(); i++){
             char ch = expr.charAt(i);
 
+            //when operator is found split to left part and right part
             if(ch == '+' || ch == '-' || ch == '*'){
 
                 List<Integer> left = diffWaysToCompute(expr.substring(0, i));
                 List<Integer> right = diffWaysToCompute(expr.substring(i+1));
 
+                //each time left and right will have a single value and ch will hold the operator 
                 for(int l : left){
                     for(int r : right){
                         if(ch == '+') ans.add(l+r);
@@ -28,6 +32,7 @@ class Solution {
             }
         }
 
+        //when the expr will not hold any operator then this base case will  hit
         if(ans.size() == 0){
             ans.add(Integer.parseInt(expr));
         }
