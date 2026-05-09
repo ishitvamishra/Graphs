efc1d2ff3701a805ddd8d6a9a0d5adfa449d90c6
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-10 00:17:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Array for Maximum Sum/partition-array-for-maximum-sum.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    public int solve(int n, int k, int arr[]) {
+        // Code here
+        int[] dp = new int[n+1];
+
+        for(int i = n - 1; i >= 0; i--){
+
+            int len = 0, max = Integer.MIN_VALUE, maxSum = Integer.MIN_VALUE;
+
+            for(int j = i; j < Math.min(i + k, n); j++){
+                len++;
+                max = Math.max(max, arr[j]);
+                int sum = len * max + dp[j+1];
+                maxSum = Math.max(maxSum, sum);
+            }
+
+            dp[i] = maxSum;
+        }
+
+        return dp[0];
+    }
+}
