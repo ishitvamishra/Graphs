5e1a0e52cbbc449611e1046facd5befaf32a267a
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-10 01:47:17 +0530

    Time: 6 ms (86.63%) | Memory: 61.9 MB (35.29%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -5,17 +5,13 @@ class Solution {
 
         int[][] dp = new int[n][m];
 
-        for(int i = 0; i < n; i++){
-            dp[i][0] = matrix[i][0];
-        }
+        for(int i = 0; i < n; i++) dp[i][0] = matrix[i][0];
 
-        for(int j = 0; j < m; j++){
-            dp[0][j] = matrix[0][j];
-        }
+        for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
 
         for(int i = 1; i < n; i++){
             for(int j = 1; j < m; j++){
-                
+
                 if(matrix[i][j] == 0) dp[i][j] = 0;
 
                 else{
@@ -31,7 +27,7 @@ class Solution {
                 sum += dp[i][j];
             }
         }
-        
+
         return sum;
     }
 }
