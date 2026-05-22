ba9f987b23431da7219ad75f93e03c6895826d8f
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-05-22 14:16:26 +0530

    Time: 40 ms (46.92%) | Memory: 108.3 MB (6.16%) - LeetSync

--- 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.java
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.java
@@ -7,13 +7,11 @@ class Solution {
         for(int x : beans) total += x;
 
         long ans = Long.MAX_VALUE;
-
         int n = beans.length;
 
         for(int i = 0; i < n; i++) {
 
             long keep = 1L * beans[i] * (n - i);
-
             long remove = total - keep;
 
             ans = Math.min(ans, remove);
