17a5bac1f575a1359710190ee4726ec1ed0b207c
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-06-24 12:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 118.9 MB (32.91%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -15,19 +15,23 @@ class Solution {
         int high = nums[0].length - 1;
 
         while(low <= high){
+            //mid column ko denote kr rha
             int mid = low + (high - low) / 2;
+            //mid wali column se maxElement ka row nikal lo
             int maxRow = findMax(nums, mid);
+            //maxElement ke upar neeche wale check krne ki need h nhi, just left aur right save krlo check krne ke liye
             int left = (mid == 0) ? -1 : nums[maxRow][mid - 1];
             int right = (mid == nums[0].length - 1) ? -1 : nums[maxRow][mid + 1];
 
+            //if curr > left and right return curr's index
             if(nums[maxRow][mid] > left && nums[maxRow][mid] > right){
                 return new int[] {maxRow, mid};
             }
-
+            //right me agar peak h toh low right me shift krdo
             if(right > nums[maxRow][mid]){
                 low = mid + 1;
             } else{
-                high = mid - 1;
+                high = mid - 1; // high left me shift krdo
             }
         }
         return new int[]{-1, -1};
