f0f413f3b418d30bf3dd7467e1b744fd32c6893d
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-07-05 14:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (13.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,40 @@
+/* Intution : the first occurence is given by lower bound directly
+the last occurence will be given by upperbound - 1 because upperbound will find the first instance 
+where arr[mid] > target and substracting 1 will give us the last occurence of target */
+
 class Solution {
-
-    private int firstOccurence(int[] nums, int target){
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] == target){
-                //for first occurence we need to keep looking at left side for first occ, hence high = mid - 1
-                ans = mid;
-                high = mid - 1;
-            } else if(nums[mid] < target){
-                low = mid + 1;
-            } else{
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    private int lastOccurence(int[] nums, int target){
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] == target){
-                //for last occurence we need to keep looking at right side for last occ, hence low = mid + 1
-                ans = mid;
-                low = mid + 1;
-            } else if(nums[mid] < target){
-                low = mid + 1;
-            } else{
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{
-            firstOccurence(nums, target),
-            lastOccurence(nums, target)
-        }; 
+        int low = 0, high = nums.length - 1;
+        int first = nums.length;
+
+        while(low <= high){
+            int mid =  low + (high - low) / 2;
+
+            if(nums[mid] >= target){
+                first = mid;
+                high = mid - 1;
+            } else{
+                low= mid + 1;
+            }
+        }
+
+        int last = nums.length;
+        low = 0; high = nums.length - 1;
+        while(low <= high){
+            int mid =  low + (high - low) / 2;
+
+            if(nums[mid] > target){
+                last = mid;
+                high = mid - 1;
+            } else{
+                low= mid + 1;
+            }
+        }
+
+        if(first == nums.length || nums[first] != target){
+            return new int[]{-1, -1};
+        }
+
+        return new int[]{first, last - 1};
     }
 }
